10a7317adf190e39cd34605b9c7a31b14475da2a
unknown <dilshadjahin@gmail.com>
2023-09-20 22:13:32 +0600

    first commit

---
+++ src/Friend.jsx
@@ -0,0 +1,9 @@
+export default function Friend({friend}){
+    return(
+        <div>
+            <h4>Name: {friend.name} </h4>
+            <p> Email: {friend.email} </p>
+        </div>
+    )
+
+}
